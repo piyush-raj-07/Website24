918b35793609a032d44ffd58f814f731ad99ad92
Naveen <146852997+NaveenSh7@users.noreply.github.com>
2025-02-11 18:42:48 +0530

    ..

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -17,14 +17,23 @@ const Sidebar = () => {
   
         </div>
       </li>
+    <a href="/quiz">
+    
       <li class="relative w-[70px] cursor-pointer uppercase transition-all duration-[0.4s] ease-[ease-out] bg-purple-400 after:absolute after:text-purple-400 after:w-[70px] after:h-full after:opacity-0 after:origin-bottom-left after:transition-all after:duration-[0.4s] after:ease-[ease-out] after:left-[70px] after:top-0 hover:translate-x-[-70px] hover:after:opacity-100 after:content-['Play'] after:leading-[88px] after:bg-[white] after:transform perspective-[400px] rotate-y-[90deg] hover:after:transform perspective-[400px] rotate-y-[0deg] scale-100 after:font-bold">
+  
         <div class="relative inline-block py-6 bg-transparent">
+         
+
+        
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" class="size-8">
     <path d="M11.25 5.337c0-.355-.186-.676-.401-.959a1.647 1.647 0 0 1-.349-1.003c0-1.036 1.007-1.875 2.25-1.875S15 2.34 15 3.375c0 .369-.128.713-.349 1.003-.215.283-.401.604-.401.959 0 .332.278.598.61.578 1.91-.114 3.79-.342 5.632-.676a.75.75 0 0 1 .878.645 49.17 49.17 0 0 1 .376 5.452.657.657 0 0 1-.66.664c-.354 0-.675-.186-.958-.401a1.647 1.647 0 0 0-1.003-.349c-1.035 0-1.875 1.007-1.875 2.25s.84 2.25 1.875 2.25c.369 0 .713-.128 1.003-.349.283-.215.604-.401.959-.401.31 0 .557.262.534.571a48.774 48.774 0 0 1-.595 4.845.75.75 0 0 1-.61.61c-1.82.317-3.673.533-5.555.642a.58.58 0 0 1-.611-.581c0-.355.186-.676.401-.959.221-.29.349-.634.349-1.003 0-1.035-1.007-1.875-2.25-1.875s-2.25.84-2.25 1.875c0 .369.128.713.349 1.003.215.283.401.604.401.959a.641.641 0 0 1-.658.643 49.118 49.118 0 0 1-4.708-.36.75.75 0 0 1-.645-.878c.293-1.614.504-3.257.629-4.924A.53.53 0 0 0 5.337 15c-.355 0-.676.186-.959.401-.29.221-.634.349-1.003.349-1.036 0-1.875-1.007-1.875-2.25s.84-2.25 1.875-2.25c.369 0 .713.128 1.003.349.283.215.604.401.959.401a.656.656 0 0 0 .659-.663 47.703 47.703 0 0 0-.31-4.82.75.75 0 0 1 .83-.832c1.343.155 2.703.254 4.077.294a.64.64 0 0 0 .657-.642Z" />
   </svg>
-  
+
         </div>
+        
       </li>
+        
+    </a>
       <li class="relative w-[70px] cursor-pointer uppercase transition-all duration-[0.4s] ease-[ease-out] bg-purple-400 after:absolute after:text-purple-400 after:w-[70px] after:h-full after:opacity-0 after:origin-bottom-left after:transition-all after:duration-[0.4s] after:ease-[ease-out] after:left-[70px] after:top-0 hover:translate-x-[-70px] hover:after:opacity-100 after:content-['Gallery'] after:leading-[88px] after:bg-[white] after:transform perspective-[400px] rotate-y-[90deg] hover:after:transform perspective-[400px] rotate-y-[0deg] scale-100 after:font-bold">
         <div class="relative inline-block py-6 bg-transparent">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" class="size-8">
@@ -88,11 +97,11 @@ content: "Home";
 line-height: 88px;
 }
 nav ul li:nth-child(2):after { 
-content: "About";
+content: "Game";
 line-height: 88px;
 }
 nav ul li:nth-child(3):after { 
-content: "Work";
+content: "Gallery";
 line-height: 85px;
 }
 nav ul li:nth-child(4):after { 
