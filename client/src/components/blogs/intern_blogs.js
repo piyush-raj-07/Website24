f55c265590f677c2d1c3ebbe9c551ffd608f1888
Himanshu <himanshukhileree@gmail.com>
2025-02-23 14:19:42 +0000

    update-blog-upvote

--- client/src/components/blogs/intern_blogs.js
+++ client/src/components/blogs/intern_blogs.js
@@ -3,6 +3,7 @@ import { useLocation } from "react-router-dom";
 import axios from "axios";
 import Fuse from "fuse.js";
 import { Link } from "react-router-dom";
+import { toast } from "react-hot-toast";
 
 const Intern = () => {
   const [blogs, setBlogs] = useState([]);
@@ -62,10 +63,10 @@ const Intern = () => {
 
   //upvote
   const handleUpvote = async (blogId) => {
-    setUpvotes((prev) => ({
-      ...prev,
-      [blogId]: (prev[blogId] || 0) + 1, // Optimistic UI update
-    }));
+    // setUpvotes((prev) => ({
+    //   ...prev,
+    //   [blogId]: (prev[blogId] || 0) + 1, // Optimistic UI update
+    // }));
 
     try {
       const response = await axios.post(`http://localhost:5000/blog/upvote/${blogId}`);
@@ -75,17 +76,18 @@ const Intern = () => {
       }));
     } catch (error) {
       console.error("Failed to upvote", error);
-      setUpvotes((prev) => ({
-        ...prev,
-        [blogId]: (prev[blogId] || 0) - 1, // Revert on failure
-      }));
+      toast.error(error.response.data.message);
+      // setUpvotes((prev) => ({
+      //   ...prev,
+      //   [blogId]: (prev[blogId] || 0) - 1, // Revert on failure
+      // }));
     }
   };
 
   useEffect(() => {
     console.log("Upvotes updated:", upvotes);
   }, [upvotes]);
-  
+
   return (
     <>
       <div className="min-h-screen bg-[#493A53] p-20 pt-8 flex flex-col items-center">
@@ -102,9 +104,8 @@ const Intern = () => {
           <input
             type="text"
             style={{ backgroundColor: "white" }}
-            placeholder={`Find blogs by ${
-              searchType === "name" ? "Name" : "Keyword"
-            }...`}
+            placeholder={`Find blogs by ${searchType === "name" ? "Name" : "Keyword"
+              }...`}
             className="w-full p-3 text-lg rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-purple-500 placeholder-gray-500 placeholder:font-semibold"
             value={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)}
@@ -122,8 +123,8 @@ const Intern = () => {
               degree={val.Auth_Degree}
               year={val.Auth_Grad_Year}
               user={val.author_id}
-              upvotes={upvotes[val._id] || 0}
-              onUpvote={handleUpvote}
+              upvotes={upvotes[val._id]}
+              onUpvote={() => handleUpvote(val._id)}
               blogId={val._id}
               onClick={() => setSelectedBlog(val)}
             />
@@ -160,7 +161,7 @@ const Intern = () => {
 };
 
 // BlogCard Component
-const BlogCard = ({ title, body, name, img, degree, year, user,upvotes, onUpvote, blogId,onClick }) => {
+const BlogCard = ({ title, body, name, img, degree, year, user, upvotes, onUpvote, blogId, onClick }) => {
   const previewText = body.length > 100 ? body.substring(0, 100) + "..." : body;
 
   return (
@@ -187,7 +188,7 @@ const BlogCard = ({ title, body, name, img, degree, year, user,upvotes, onUpvote
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-8 h-8">
           <path strokeLinecap="round" strokeLinejoin="round" d="M6.633 10.25c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 0 1 2.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 0 0 .322-1.672V2.75a.75.75 0 0 1 .75-.75 2.25 2.25 0 0 1 2.25 2.25c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282m0 0h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 0 1-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 0 0-1.423-.23H5.904m10.598-9.75H14.25M5.904 18.5c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 0 1-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 9.953 4.167 9.5 5 9.5h1.053c.472 0 .745.556.5.96a8.958 8.958 0 0 0-1.302 4.665c0 1.194.232 2.333.654 3.375Z" />
         </svg>
-        <span>{upvotes[blogId] || 0}</span>
+        <span className="text-sm font-semibold">{upvotes || 0}</span>
       </button>
 
       {/* Blog Content */}
