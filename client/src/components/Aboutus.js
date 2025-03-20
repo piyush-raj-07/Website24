06d764c377b4490daced9a6364f2f6152be2cbea
harshitab04 <hbankure04@gmail.com>
2025-03-20 23:49:58 +0530

    background gradient 2

--- client/src/components/Aboutus.js
+++ client/src/components/Aboutus.js
@@ -27,7 +27,8 @@ const AboutUs = () => {
     }, []);
 
     return (
-        <div className="flex flex-col md:flex-row items-center justify-center min-h-screen bg-gradient-to-b from-[#634669] from-35% to-[#a45dbb] p-8">
+        <div className="flex flex-col md:flex-row items-center justify-center min-h-screen bg-gradient-to-b from-[#38283C] via-[#6d3f7e] to-[#a45dbb]
+ p-8">
             
             <div className="w-full md:w-1/2">
                 <img
